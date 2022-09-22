e286b19a434a79e4e2d70eb0797619c4d290974a
Matthew Woodington <matthewwoodington@Matthews-MacBook-Pro.local>
2022-09-22 14:41:20 -0400

    adds class structure

---
+++ js/main.js
@@ -0,0 +1,54 @@
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+  }
+}
+
+class Hero extends Character {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+class Villian extends Character {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+class Hunter extends Hero {
+  constructor() {
+    super();
+    this.name = "Hunter";
+    this.attacks = [{ punch: 10 }, { slash: 15 }, { magic: 10 }];
+  }
+}
+
+class Titan extends Hero {
+  constructor() {
+    super();
+    this.name = "Titan";
+    this.attacks = [{ punch: 20 }, { slash: 10 }, { magic: 5 }];
+  }
+}
+
+class Warlock extends Hero {
+  constructor() {
+    super();
+    this.name = "Warlock";
+    this.attacks = [{ punch: 10 }, { slash: 5 }, { magic: 20 }];
+  }
+}
+
+class Fallen extends Villian {
+  constructor() {
+    super();
+    this.name = "Fallen";
+    this.attacks = [{ bite: 10 }, { grab: 5 }, { shoot: 20 }];
+  }
+}
+
+const hunter = new Hunter();
