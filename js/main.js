51436b36d7d1f7e59b2d77a450931c3631a788e1
Matthew Woodington <matthewwoodington@Matthews-MacBook-Pro.local>
2022-09-23 14:52:33 -0400

    adds updated displays

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const selectCharacter = document.querySelector(".character");
 const startBtn = document.querySelector(".start-btn");
 const attackBtns = document.querySelectorAll(".attack-button");
-let gameOver= false;
+const villainHealth = document.querySelector(".villainPower");
+const heroHealth = document.querySelector(".heroPower");
+const heroText = document.querySelector(".hero-text");
+const villainText = document.querySelector(".villain-text");
+
+let gameOver = false;
+
+attackBtns.forEach((attackBtn) => {
+  attackBtn.disabled = true;
+});
 
 class Character {
   constructor(name) {
@@ -138,8 +147,7 @@ class Game {
   }
 
   getActiveHero() {
-    let characterOutput =
-      selectCharacter.options[selectCharacter.selectedIndex].value;
+    let characterOutput = selectCharacter.options[selectCharacter.selectedIndex].value;
     console.log(characterOutput);
 
     if (characterOutput === "Hunter") {
@@ -168,47 +176,80 @@ class Game {
   }
 
   pairAttackValue(btnValue) {
-    let chosenAttack = this.hero.attacks.find(
-      ({ attack }) => attack === `${btnValue}`
-    );
-    if( this.villain.health <= 0 || gameOver){
-      gameOver = true;
-      return;
-    }
-    console.log(chosenAttack.value, "hero attack");
+    let chosenAttack = this.hero.attacks.find(({ attack }) => attack === `${btnValue}`);
+    // console.log(chosenAttack.value, "hero attack");
     this.villain.health -= chosenAttack.value;
-    document.querySelector(".villainPower").innerHTML = this.villain.health;
-    console.log(this.villain.health, "villain health");
-    if (this.villain.health <= 0) {
-      this.gameOver = true;
+    // console.log(this.villain.health, "villain health");
+    heroText.value = `${this.hero.name} uses ${chosenAttack.attack} and does ${chosenAttack.value} damage!`;
+
+    if (this.villain.health <= 0 || gameOver) {
+      gameOver = true;
     }
+    game.checkGameOver();
+    game.updateVillainHealth();
 
     setTimeout(() => {
-      if( this.hero.health <= 0 || gameOver){
-        gameOver = true;
-        return
-      }
       const random = Math.floor(Math.random() * this.villain.attacks.length);
-      let villainAttackValue = this.villain.attacks[random].value;
-      console.log(villainAttackValue, "villain attack");
+      let villainAttack = this.villain.attacks[random];
+      let villainAttackValue = villainAttack.value;
+      // console.log(villainAttackValue, "villain attack");
       this.hero.health -= villainAttackValue;
-      document.querySelector(".heroPower").innerHTML = this.hero.health;
-      console.log(this.hero.health, "hero health");
-      if (this.hero.health <= 0) {
-        this.gameOver = true;
+      // console.log(this.hero.health, "hero health");
+      villainText.value = `${this.villain.name} uses ${villainAttack.attack} and does ${villainAttackValue} damage!`;
+
+      if (this.hero.health <= 0 || gameOver) {
+        gameOver = true;
       }
-      document.querySelector(".heroPower").innerHTML = this.villain.health;
-      console.log(this.hero.health, "hero health");
+      game.checkGameOver();
+      game.updateHeroHealth();
     }, 1000);
+  }
+
+  updateHeroHealth() {
+    heroHealth.innerHTML = this.hero.health;
+  }
+
+  updateVillainHealth() {
+    villainHealth.innerHTML = this.villain.health;
+  }
+
+  checkGameOver() {
+    if (!gameOver) {
+      return;
+    } else if (this.hero.health <= 0) {
+      heroText.value = "Hero Loses!";
+      this.hero.health = 0;
+      villainText.value = "Villain Wins!";
+
+      attackBtns.forEach((attackBtn) => {
+        attackBtn.disabled = true;
+      });
+    } else if (this.villain.health <= 0) {
+      heroText.value = "Hero Wins!";
+      this.villain.health = 0;
+      villainText.value = "Villain Loses!";
+
+      attackBtns.forEach((attackBtn) => {
+        attackBtn.disabled = true;
+      });
+    }
   }
 }
 
 const game = new Game();
 
 startBtn.addEventListener("click", () => {
-  gameOver= false;
+  gameOver = false;
   game.getActiveHero();
   game.getActiveVillain();
+  game.updateHeroHealth();
+  game.updateVillainHealth();
+
+  attackBtns.forEach((attackBtn) => {
+    attackBtn.disabled = false;
+  });
+  heroText.value = "";
+  villainText.value = "";
 });
 
 attackBtns.forEach((attackBtn) => {
